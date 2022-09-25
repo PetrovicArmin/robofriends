987671fba1c3d62958460a1016f45c01feb3fa03
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-09-25 17:29:34 +0200

    Updates

---
+++ precache-manifest.a3f634881a855184e25d4e7e58848eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ab16cb66f55884a490f",
+    "url": "/robofriends/static/css/main.abd6b16e.chunk.css"
+  },
+  {
+    "revision": "8ab16cb66f55884a490f",
+    "url": "/robofriends/static/js/main.8ab16cb6.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/robofriends/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "4f16ec1bf12ade1440cb",
+    "url": "/robofriends/static/css/2.df9b6d86.chunk.css"
+  },
+  {
+    "revision": "4f16ec1bf12ade1440cb",
+    "url": "/robofriends/static/js/2.4f16ec1b.chunk.js"
+  },
+  {
+    "revision": "e1998df22156d2c9f133",
+    "url": "/robofriends/static/js/runtime~main.e1998df2.js"
+  },
+  {
+    "revision": "646de268faf13c8cac5ef8562c3d8d1d",
+    "url": "/robofriends/index.html"
+  }
+];
